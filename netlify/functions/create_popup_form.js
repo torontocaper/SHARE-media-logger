e509ce5f11793305860a87971a1b3e10b7a7a4e6
Adam <torontocaper@gmail.com>
2026-07-06 15:20:26 -0400

    update stylesheet fonts

--- netlify/functions/create_popup_form.js
+++ netlify/functions/create_popup_form.js
@@ -2,15 +2,10 @@ const fs = require("fs");
 const path = require("path");
 
 exports.handler = async function (event) {
-  console.log("Someone used the bookmarklet.")
   const params = event.queryStringParameters || {};
-  console.log("Query string parameters:", params);
   const article_name = params.article_name || "";
-  console.log("Article Name: " + article_name);
   const link_to_article = params.link_to_article || "";
-  console.log("Link to Article: " + link_to_article);
 
-  console.log("Loading popup_form.html template...");
   const templatePath = path.join(
     __dirname,
     "popup_form.html"
@@ -40,18 +35,3 @@ function escapeHtml(value) {
     .replaceAll('"', "&quot;")
     .replaceAll("'", "&#039;");
 }
-
-// Convert date strings into something the HTML date input can understand.
-/* function formatDateForInput(value) {
-  if (!value) {
-    return "";
-  }
-
-  const date = new Date(value);
-
-  if (Number.isNaN(date.getTime())) {
-    return "";
-  }
-
-  return date.toISOString().slice(0, 10);
-} */
