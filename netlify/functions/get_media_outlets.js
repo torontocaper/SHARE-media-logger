0abc59a21a8ef5aa4311ad80d5c7ad69c556b759
Adam <torontocaper@gmail.com>
2026-08-06 11:56:45 -0400

    remove unnecessary console logs

--- netlify/functions/get_media_outlets.js
+++ netlify/functions/get_media_outlets.js
@@ -4,40 +4,26 @@ const { authenticate_salesforce } = require("./lib/authorize_salesforce_app");
 
 exports.handler = async function () {
   const sf_access_token = await authenticate_salesforce();
-  console.log("Salesforce access token:", sf_access_token);
+
   const sf_query = `
     SELECT Id, Name
     FROM Account
     WHERE Category__c = 'Media'
     ORDER BY Name
   `.replace(/\s+/g, " ").trim();
-  console.log("Salesforce query:", sf_query);
+
   const path =
     `/services/data/${sf_api_version}/query` +
     `?q=${encodeURIComponent(sf_query)}`;
-  console.log("Salesforce query path:", path);
-  const response = await fetch(`${sf_instance_url}${path}`, {
+
+    const response = await fetch(`${sf_instance_url}${path}`, {
     method: "GET",
     headers: {
       Authorization: `Bearer ${sf_access_token}`,
     },
   });
-  console.log("Salesforce query response status:", response.status);
+
   const result = await response.json();
-  console.log("Salesforce query response:", result);
-/*   if (!response.ok) {
-    console.error("Salesforce query failed:", result);
-      return {
-        statusCode: response.status,
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          success: false,
-          error: result,
-        }),
-      };
-    } */
 
   const outlets = result.records.map(
       ({ Id, Name }) => ({
