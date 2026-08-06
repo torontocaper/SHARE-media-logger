56df433c2e96b5f6912abb35b668f95ac7754cef
Adam <torontocaper@gmail.com>
2026-08-06 12:57:03 -0400

    delete commented-out code

--- netlify/functions/lib/authorize_salesforce_app.js
+++ netlify/functions/lib/authorize_salesforce_app.js
@@ -5,12 +5,6 @@ async function authenticate_salesforce() {
   const sf_consumer_id = process.env.SF_CONSUMER_ID;
   const sf_consumer_secret = process.env.SF_CONSUMER_SECRET;
 
-/*   if (!clientId || !clientSecret) {
-    throw new Error(
-      "Missing SF_CONSUMER_ID or SF_CONSUMER_SECRET."
-    );
-  } */
-
   const body = new URLSearchParams({
     grant_type: "client_credentials",
     client_id: sf_consumer_id,
@@ -30,43 +24,11 @@ async function authenticate_salesforce() {
 
   const result = await response.json();
   console.log("Salesforce OAuth response:", result);
-/*
-  if (!response.ok) {
-    console.error("Salesforce OAuth response:", result);
-
-    throw new Error(
-      result.error_description ||
-      result.error ||
-      "Salesforce authentication failed."
-    );
-  } */
 
   return result.access_token;
-/*     instanceUrl: result.instance_url,
- */
+
 }
 
 module.exports = {
   authenticate_salesforce,
-  //salesforceFetch,
 };
-
-
-/* async function salesforceFetch(path, options = {}) {
-  const { accessToken, instanceUrl } =
-    await authenticateSalesforce();
-
-  const headers = {
-    ...(options.headers || {}),
-    Authorization: `Bearer ${accessToken}`,
-  };
-
-  if (options.body && !headers["Content-Type"]) {
-    headers["Content-Type"] = "application/json";
-  }
-
-  return fetch(`${instanceUrl}${path}`, {
-    ...options,
-    headers,
-  });
-} */
