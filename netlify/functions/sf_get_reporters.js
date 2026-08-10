08ebd973c14d82cf04eb0d2500809fd2dfb8cf22
Adam <torontocaper@gmail.com>
2026-08-10 15:56:59 -0400

    restore 'get' files to functions directory

--- netlify/functions/sf_get_reporters.js
+++ netlify/functions/sf_get_reporters.js
@@ -1,6 +1,6 @@
 const sf_api_version = process.env.SF_API_VERSION;
 const sf_instance_url = process.env.SF_INSTANCE_URL;
-const { authenticate_salesforce } = require("../lib/authorize_salesforce_app");
+const { authenticate_salesforce } = require("./lib/authorize_salesforce_app");
 
 exports.handler = async function () {
   const sf_access_token = await authenticate_salesforce();
